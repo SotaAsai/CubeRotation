020c000c3c17352e156379063c1c98af56d7afa8
浅井聡太 <>
2018-12-19 22:22:49 +0900

    コメント削除

--- Assets/Scripts/RotationScript.cs
+++ Assets/Scripts/RotationScript.cs
@@ -10,25 +10,18 @@ public class RotationScript : MonoBehaviour {
     InputField inputY;
     InputField inputZ;
 
-    // Use this for initialization
     void Start () {
         inputAngle = GameObject.FindWithTag("inputAngle").GetComponent<InputField>();
         inputX = GameObject.FindWithTag("inputX").GetComponent<InputField>();
         inputY = GameObject.FindWithTag("inputY").GetComponent<InputField>();
         inputZ = GameObject.FindWithTag("inputZ").GetComponent<InputField>();
     }
-    
-    // Update is called once per frame
+
     void Update () {
         
     }
 
     public void rotateCube() {
-        
-        //InputField inputAngle = GameObject.FindWithTag("inputAngle").GetComponent<InputField>();
-        //InputField inputX = GameObject.FindWithTag("inputX").GetComponent<InputField>();
-        //InputField inputY = GameObject.FindWithTag("inputY").GetComponent<InputField>();
-        //InputField inputZ = GameObject.FindWithTag("inputZ").GetComponent<InputField>();
 
         float angle = inputAngle.text.Equals("") ? 0f : float.Parse(inputAngle.text);
         float x = inputX.text.Equals("") ? 0f : float.Parse(inputX.text);
